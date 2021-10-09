993910ee3c57377e25fb67c889b80e084a12f2a8
Jan Kudlicka <github@kudlicka.eu>
2021-10-09 15:41:43 +0200

    Added the TOML configuration files.

--- benchmark-suite/benchmarks/ppl/yap-dengue/birch/logz.py
+++ benchmark-suite/benchmarks/ppl/yap-dengue/birch/logz.py
@@ -1,5 +1,5 @@
 import json
 
-data = json.load(open('output/experiment0.json'))
+data = json.load(open('output/output.json'))
 for item in data:
     print(item['lweight'])
--- benchmark-suite/benchmarks/ppl/yap-dengue/birch/test.sh
+++ benchmark-suite/benchmarks/ppl/yap-dengue/birch/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-BIRCH_MODE="release" birch sample --config config/experiment0.json --quiet true && python logz.py
+BIRCH_MODE="release" birch sample --config config/config20000.json --quiet true && python logz.py
